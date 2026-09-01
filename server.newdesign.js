db711937d542032a9221f36b7dda6383eb7f3bc3
haddad258 <haddadrafik258@gmail.com>
2026-09-01 22:46:21 +0000

    update

--- server.newdesign.js
+++ server.newdesign.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 4006;
+const PORT = process.env.PORT || 4001;
 
-app.use(express.static(path.join(__dirname, "hotel-booking-newdesign/dist")));
+app.use(express.static(path.join(__dirname, "hotel-booking-admin/dist")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "hotel-booking-newdesign/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "hotel-booking-admin/dist", "index.html"));
 });
 
 app.listen(PORT, () => {
